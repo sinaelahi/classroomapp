7cb0cef8cc84b2fb6fc520d4465e53d5e183f888
Sina <sinaelahu@gmail.com>
2026-07-17 13:55:29 +0330

    main file updated with new web service

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 import 'app.dart';
@@ -7,17 +8,22 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.initDependencies();
 
-  await windowManager.ensureInitialized();
-  const windowOptions = WindowOptions(
-    size: Size(1280, 800),
-    minimumSize: Size(1024, 700),
-    center: true,
-    title: 'Dershane Yönetim Paneli',
-  );
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
-  });
+  // window_manager sadece masaüstünde (Windows/macOS/Linux) çalışır.
+  // Web'de kIsWeb true olduğu için bu bloğu atlıyoruz, aksi halde
+  // uygulama tarayıcıda açılmadan hata verir.
+  if (!kIsWeb) {
+    await windowManager.ensureInitialized();
+    const windowOptions = WindowOptions(
+      size: Size(1280, 800),
+      minimumSize: Size(1024, 700),
+      center: true,
+      title: 'Dershane Yönetim Paneli',
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
 
   runApp(const DershaneApp());
 }
